e2cb9a2e901acc93db76601f06121a958a7a0c40
walle13 <lcz.13zxj@qq.com>
2019-11-15 22:10:57 +0800

    new test

---
+++ Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace SMC_600Test
+{
+    partial class CB_Start
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.SB_Stop = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(175, 121);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(128, 74);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "启动";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.SB_Start_Click);
+            // 
+            // SB_Stop
+            // 
+            this.SB_Stop.Location = new System.Drawing.Point(413, 121);
+            this.SB_Stop.Name = "SB_Stop";
+            this.SB_Stop.Size = new System.Drawing.Size(128, 74);
+            this.SB_Stop.TabIndex = 1;
+            this.SB_Stop.Text = "停止";
+            this.SB_Stop.UseVisualStyleBackColor = true;
+            this.SB_Stop.Click += new System.EventHandler(this.SB_Stop_Click);
+            // 
+            // CB_Start
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.SB_Stop);
+            this.Controls.Add(this.button1);
+            this.Name = "CB_Start";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.CB_Start_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        public System.Windows.Forms.Button button1;
+        public System.Windows.Forms.Button SB_Stop;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Leadshine.SMC.IDE.Motion;
+
+
+namespace SMC_600Test
+{
+    public partial class CB_Start : Form
+    {
+        public CB_Start()
+        {
+            InitializeComponent();
+        }
+
+        private void SB_Start_Click(object sender, EventArgs e)
+        {
+            ushort CardNo = 0;          //卡号
+            ushort axis = 0;            //运动轴号u
+            double start_speed = 0;     //启动速度
+            double speed = 1000;        //最大运行速度
+            double stop_speed = 0;      //停止速度
+            double tacc = 0.1;          //加速时间
+            double tdec = 0.1;      //减速时间
+            double s_pare = 0.05;   //s形平滑系数
+            double dist = 10000;    //运动距离
+            LTSMC.smc_set_profile_unit(CardNo, axis, start_speed, speed, tacc, tdec, stop_speed);//设置速度参数
+            LTSMC.smc_set_s_profile(CardNo, axis, 0, s_pare);   //设置S平滑系数
+            LTSMC.smc_pmove_unit(CardNo, axis, dist, 0);    //启动定长运动
+                
+        }
+
+        private void SB_Stop_Click(object sender, EventArgs e)
+        {
+            ushort CardNo = 0;          //卡号
+            ushort axis = 0;            //运动轴号u
+            ushort mode = 0;     //停止模式，0；减速停止，1；紧急停止
+            LTSMC.smc_stop(CardNo, axis, mode);
+        }
+
+        private void CB_Start_Load(object sender, EventArgs e)
+        {
+            ushort CardNo = 0;
+            short res = LTSMC.smc_board_init(CardNo, 2, "192.168.2.77", 0);
+            if (res != 0)
+            {
+                MessageBox.Show(string.Format("连接控制器失败，错误码：【0】",res),"错误");
+            }
+            
+        }
+    }
+}
